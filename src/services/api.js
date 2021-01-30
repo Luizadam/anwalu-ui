9dce40076babdfc9ede6af0cce2132d3860241a6
Luizadam <luizadaam@gmail.com>
2021-01-30 12:51:04 +0700

    fix url

--- src/services/api.js
+++ src/services/api.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export default axios.create({
   // Base api url
-  baseURL: 'https://anwalu-backend.herokuapp.com/'
+  baseURL: 'https://anwalu-backend.herokuapp.com'
 });
